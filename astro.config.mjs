3393f73aea41c14ff5b43a11e47cdcba09e51371
Théo Gianella <theo.gianella@proton.me>
2026-04-13 18:30:33 +0200

    chore: add stylelint + fix issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tgianella.dev', // replace with your domain
+  site: 'https://tgianella.dev',
   integrations: [
-    react(),
     mdx(),
     sitemap({
       i18n: {
