d8adde05596ef69be30edf676655bc48e9a95b24
Théo Gianella <theo.gianella@proton.me>
2026-04-13 18:15:26 +0200

    chore: install eslint + fix issues

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import eslintPluginAstro from "eslint-plugin-astro";
+
+export default defineConfig([
+  { ignores: ["**/*.d.ts", ".astro/", "dist/"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {...globals.browser, ...globals.node}
+    }
+  },
+  tseslint.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "error",
+    },
+  },
+  ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginAstro.configs['jsx-a11y-recommended'],
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
+]);
